7a3aaec360a807afa362ad419e6e3a0bf8db6e0e
DzusDaTo <daviddzusov946@gmail.com>
2025-02-16 15:38:10 +0300

    Commit celery

--- src/core/my_celery.py
+++ src/core/my_celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('core')
+app = Celery('core', broker='redis://localhost:6379/0')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
